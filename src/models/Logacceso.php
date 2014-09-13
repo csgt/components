9a6b101dbf7709e7fcf9b9fb62fb71929de29982
root <root@serverdev.cs.local>
2014-09-13 09:52:36 -0600

    Se agrego el modelo de log

---
+++ src/models/Logacceso.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Logacceso {
+	protected $primaryKey = 'accesoid';
+	protected $table = 'logacceso';
+
+	public static function getUltimoAcceso() {
+		return DB::table('logacceso')
+			->select('fechalogin','ip')
+			->where('usuarioid',Auth:id())
+			->orderBy('fechalogin','DESC')
+			->first();
+	}
+}
