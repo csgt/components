6aec15f3aa4fbc8deeb78a4d996b0e65fc95c8ba
Jose Galindo (Desktop) <jgalindo@cs.com.gt>
2015-01-26 08:58:19 -0600

    Facade a Components y numero a letras como funcion.

--- src/Csgt/Components/Components.php
+++ src/Csgt/Components/Components.php
@@ -4,6 +4,15 @@ namespace Csgt\Components;
 use DB, Auth;
 
 class Components {
+	private $unidades = ['','UN ','DOS ','TRES ','CUATRO ','CINCO ','SEIS ','SIETE ','OCHO ','NUEVE ','DIEZ ',
+    'ONCE ','DOCE ','TRECE ','CATORCE ','QUINCE ','DIECISEIS ','DIECISIETE ','DIECIOCHO ','DIECINUEVE ','VEINTE '];
+  private $decenas  = ['VENTI','TREINTA ','CUARENTA ','CINCUENTA ','SESENTA ','SETENTA ','OCHENTA ','NOVENTA ','CIEN '];
+  private $centenas = ['CIENTO ','DOSCIENTOS ','TRESCIENTOS ','CUATROCIENTOS ','QUINIENTOS ',
+    'SEISCIENTOS ','SETECIENTOS ','OCHOCIENTOS ','NOVECIENTOS '];
+  private $monedas = [
+        ['country' => 'Guatemala', 'currency' => 'Q', 'singular' => 'QUETZAL', 'plural' => 'QUETZALES', 'symbol', 'Q'],
+        ['country' => 'Estados Unidos', 'currency' => 'USD', 'singular' => 'DÓLAR', 'plural' => 'DÓLARES', 'symbol', 'US$']];
+
 	public static function getMenuForRole() {
 		$query = 'SELECT * FROM authmenu WHERE ruta IN (
 			SELECT 
@@ -32,4 +41,88 @@ class Components {
 		return DB::select(DB::raw($query));
 	}
 
+	public static function numerosALetras($aNumero, $aMoneda=null) {  
+    if ($aMoneda !== null) {
+      try {
+        $moneda = array_filter($this->MONEDAS, function($m) use ($aMoneda) {
+        	return ($m['currency'] == $aMoneda);
+        });
+
+        $moneda = array_values($moneda);
+
+        if (count($moneda) <= 0) {
+          throw new Exception("Tipo de moneda inválido");
+          return;
+        }
+
+        if ($aNumero < 2) {
+        	$moneda = $moneda[0]['singular'];
+        } else {
+          $moneda = $moneda[0]['plural'];
+        }
+      } catch (Exception $e) {
+        echo $e->getMessage();
+        return;
+      }
+    } else {
+      $moneda = " ";
+    }
+
+    $converted = '';
+
+    if (($aNumero < 0) || ($aNumero > 999999999)) {
+        return 'No es posible convertir el numero a letras';
+    }
+
+		$aNumeroStr     = (string) $aNumero;
+		$aNumeroStrFill = str_pad($aNumeroStr, 9, '0', STR_PAD_LEFT);
+		$millones       = substr($aNumeroStrFill, 0, 3);
+		$miles          = substr($aNumeroStrFill, 3, 3);
+		$cientos        = substr($aNumeroStrFill, 6);
+
+    if (intval($millones) > 0) {
+      if ($millones == '001') 
+        $converted .= 'UN MILLON ';
+      else if (intval($millones) > 0) 
+        $converted .= sprintf('%sMILLONES ', $this->convertGroup($millones));
+    }
+    
+    if (intval($miles) > 0) {
+      if ($miles == '001') 
+        $converted .= 'MIL ';
+      else if (intval($miles) > 0)
+        $converted .= sprintf('%sMIL ', $this->convertGroup($miles));
+    }
+
+    if (intval($cientos) > 0) {
+      if ($cientos == '001') 
+        $converted .= 'UN ';
+      else if (intval($cientos) > 0) 
+        $converted .= sprintf('%s ', $this->convertGroup($cientos));
+    }
+
+    $converted .= $moneda;
+    return trim($converted);
+  }
+
+  private function convertGroup($n) {
+    $output = '';
+    if ($n == '100') 
+      $output = "CIEN ";
+    else if ($n[0] !== '0') 
+      $output = $this->centenas[$n[0] - 1];   
+
+    $k = intval(substr($n,1));
+
+    if ($k <= 20) {
+      $output .= $this->unidades[$k];
+    } 
+    else {
+      if(($k > 30) && ($n[2] !== '0')) 
+        $output .= sprintf('%sY %s', $this->decenas[intval($n[1]) - 2], $this->unidades[intval($n[2])]);
+      else 
+        $output .= sprintf('%s%s', $this->decenas[intval($n[1]) - 2], $this->unidades[intval($n[2])]);
+    }
+    return $output;
+  }
 }
---
+++ src/Facades/Components.php
@@ -0,0 +1,7 @@
+<?php namespace namespace Csgt\Components\Facades;
+ 
+use Illuminate\Support\Facades\Facade;
+ 
+class Components extends Facade {
+  protected static function getFacadeAccessor() { return 'components'; }
+}
