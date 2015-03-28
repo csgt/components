e789dd3e9078d6119f7ad92d5ebe29f13d05d751
Jose Galindo (Desktop) <jgalindo@cs.com.gt>
2015-03-27 19:27:07 -0600

    Se cambio la ruta a cancerbero/generarmodulopermisos

--- src/routes.php
+++ src/routes.php
@@ -7,5 +7,5 @@
 	Route::group(array('before' => array('auth','god')), function() {
 		Route::resource('cancerbero/modulos', 'modulosController');
 		Route::resource('cancerbero/permisos', 'permisosController');
-		Route::resource('generarmodulopermisos','generarmodulopermisosController', array('only'=>array('index','store')));
+		Route::resource('cancerbero/generarmodulopermisos','generarmodulopermisosController', array('only'=>array('index','store')));
 	});
