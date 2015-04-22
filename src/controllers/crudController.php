96e40ec668fcd738ddc8f097e150a69b1af5900a
Jose Galindo (Desktop) <jgalindo@cs.com.gt>
2015-04-22 14:30:20 -0600

    static a los metodos de CRUD

--- src/controllers/crudController.php
+++ src/controllers/crudController.php
@@ -2,31 +2,31 @@
 
 class crudController extends BaseController {
 
-	public function index() {
+	public static function index() {
 		return Crud::index();
 	}
 
-	public function create() {
+	public static function create() {
 		return Crud::create(0);
 	}
 
-	public function store() {
+	public static function store() {
 		return Crud::store();
 	}
 
-	public function show($id) {
+	public static function show($id) {
 		return Crud::getData($id);
 	}
 
-	public function edit($id) {
+	public static function edit($id) {
 		return Crud::create($id);
 	}
 
-	public function update($id) {
+	public static function update($id) {
 		return Crud::store($id);
 	}
 
-	public function destroy($id) {
+	public static function destroy($id) {
 		return Crud::destroy($id);
 	}
 	
