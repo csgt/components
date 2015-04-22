6e117ed0d5f24418da3e38633656d810a0a3072d
Jose Galindo (Desktop) <jgalindo@cs.com.gt>
2015-04-22 15:23:39 -0600

    Rollback de static methods

--- src/controllers/crudController.php
+++ src/controllers/crudController.php
@@ -2,31 +2,31 @@
 
 class crudController extends BaseController {
 
-	public static function index() {
+	public function index() {
 		return Crud::index();
 	}
 
-	public static function create() {
+	public function create() {
 		return Crud::create(0);
 	}
 
-	public static function store() {
+	public function store() {
 		return Crud::store();
 	}
 
-	public static function show($id) {
+	public function show($id) {
 		return Crud::getData($id);
 	}
 
-	public static function edit($id) {
+	public function edit($id) {
 		return Crud::create($id);
 	}
 
-	public static function update($id) {
+	public function update($id) {
 		return Crud::store($id);
 	}
 
-	public static function destroy($id) {
+	public function destroy($id) {
 		return Crud::destroy($id);
 	}
 	
