64fa9a80fb7687ba52b56299a78bd123db4b3af7
Jose Galindo <jgalindo@cs.com.gt>
2017-03-21 20:58:29 -0600

    Middleware para arreglar menu selected

--- src/Http/Middleware/MenuMW.php
+++ src/Http/Middleware/MenuMW.php
@@ -1,18 +1,20 @@
 <?php 
 namespace Csgt\Components\Http\Middleware;
 
-use Closure, Csgt\Components\Authmenu, Menu, Cancerbero, Session;
+use Closure, Csgt\Components\Authmenu, Menu, Cancerbero, Session, Log;
 
 class MenuMW {
 	public function handle($request, Closure $next) {
 	  if(!Session::has('menu-collection')){
-			
 			$elAuthMenu = new Authmenu;
 			$elAuthMenu->getMenuForRole();
 		}	
 		$elMenu = new Menu;
 		$menuCollection = Session::get('menu-collection');
-		//$selected = Session::
+		$route = $request->route()->getName();
+		$route = substr($route,0,strrpos($route,'.')) . '.index';
+		
+		Session::put('menu-selected', $route);
 		Session::put('menu', $elMenu->generarMenu($menuCollection));
 		return $next($request);
 	}
