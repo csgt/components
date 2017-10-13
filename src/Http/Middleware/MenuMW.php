a8796771a7170d5758ccd2ebaeb0cb0b095e36d6
Jose Galindo <jgalindo@cs.com.gt>
2017-10-13 11:44:56 -0600

    Validar en middleware del menu que el usuario este autenticado.

--- src/Http/Middleware/MenuMW.php
+++ src/Http/Middleware/MenuMW.php
@@ -1,21 +1,29 @@
-<?php 
+<?php
 namespace Csgt\Components\Http\Middleware;
 
-use Closure, Csgt\Components\Authmenu, Menu, Cancerbero, Session, Log;
+use Closure;
+use Csgt\Components\Authmenu;
+use Menu;
+use Session;
+use Auth;
 
-class MenuMW {
-	public function handle($request, Closure $next) {
-	  if(!Session::has('menu-collection')){
-			$elAuthMenu = new Authmenu;
-			$elAuthMenu->getMenuForRole();
-		}	
-		$elMenu = new Menu;
-		$menuCollection = Session::get('menu-collection');
-		$route = $request->route()->getName();
-		$route = substr($route,0,strrpos($route,'.')) . '.index';
-		
-		Session::put('menu-selected', $route);
-		Session::put('menu', $elMenu->generarMenu($menuCollection));
-		return $next($request);
-	}
+class MenuMW
+{
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check()) {
+            if (!Session::has('menu-collection')) {
+                $elAuthMenu = new Authmenu;
+                $elAuthMenu->getMenuForRole();
+            }
+            $elMenu = new Menu;
+            $menuCollection = Session::get('menu-collection');
+            $route = $request->route()->getName();
+            $route = substr($route, 0, strrpos($route, '.')) . '.index';
+
+            Session::put('menu-selected', $route);
+            Session::put('menu', $elMenu->generarMenu($menuCollection));
+        }
+        return $next($request);
+    }
 }
