8ceb08094a20eb15146371760256ef6e36ddd951
Jose Galindo <jgalindo@softlogic.com.gt>
2020-10-28 19:47:06 -0600

    Quitar views inexistentes. PSR2

--- src/ComponentsServiceProvider.php
+++ src/ComponentsServiceProvider.php
@@ -1,9 +1,9 @@
 <?php
 namespace Csgt\Components;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Foundation\AliasLoader;
 use Illuminate\Routing\Router;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Support\ServiceProvider;
 
 class ComponentsServiceProvider extends ServiceProvider
 {
@@ -15,24 +15,23 @@ class ComponentsServiceProvider extends ServiceProvider
         AliasLoader::getInstance()->alias('Components', 'Csgt\Components\Components');
 
         $this->mergeConfigFrom(__DIR__ . '/config/csgtcomponents.php', 'csgtcomponents');
-        $this->loadViewsFrom(__DIR__ . '/resources/views/', 'csgtcomponents');
 
         $router->aliasMiddleware('menu', '\Csgt\Components\Http\Middleware\MenuMW');
         $router->aliasMiddleware('god', '\Csgt\Components\Http\Middleware\GodMW');
 
         $this->publishes([
-            __DIR__.'/config/csgtcomponents.php' => config_path('csgtcomponents.php'),
+            __DIR__ . '/config/csgtcomponents.php' => config_path('csgtcomponents.php'),
         ], 'config');
     }
 
     public function register()
     {
         $this->commands([
-            Console\MakeComponentsCommand::class
+            Console\MakeComponentsCommand::class,
         ]);
 
         $this->commands([
-            Console\MakeDockerCommand::class
+            Console\MakeDockerCommand::class,
         ]);
 
         $this->app->singleton('components', function ($app) {
